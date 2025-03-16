e83b02a4f166df4ccb98de849e2aaf6c19c6db06
Robert Krause <robertkrause@gmx.at>
2025-03-16 15:52:58 +0100

    Initial Commit

---
+++ Arduino Control/Arduino_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arduino_ControlApp.swift
+//  Arduino Control
+//
+//  Created by Robert Krause on 16.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Arduino_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Arduino Control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Arduino Control
+//
+//  Created by Robert Krause on 16.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
